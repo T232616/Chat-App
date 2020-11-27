c6f34bc3c6b58fb9f0fc18adca974e85b9045b0c
Tejas Jhamnani <tejasj2909@gmail.com>
2020-11-27 17:30:53 +0530

    Chat App

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,11 +1,8 @@
 <?php
 
-// $server = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "forum";
-$server = "remotemysql.com";
-$username = "kmXCKoyAZL";
-$password = "nJr8p8RgS1";
-$database = "kmXCKoyAZL";
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "forum";
+
 $conn = mysqli_connect($server, $username, $password, $database);
