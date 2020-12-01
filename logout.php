23664904a3035b5b40b77b3d6ef84a34b4a4bdf2
Tejas Jhamnani <tejasj2909@gmail.com>
2020-12-01 18:02:34 +0530

    Chat App

--- logout.php
+++ logout.php
@@ -1,7 +1,8 @@
-<?php session_start();?>
+
 <?php include 'partials/_dbconnect.php'?>
 <?php include 'partials/_header.php'?>
 <?php
+session_start();
 session_unset();
 session_destroy();
 header("Location:/chat-app");
