1f54252126cefb8de157537a3d712a6934b4ae67
Tejas Jhamnani <tejasj2909@gmail.com>
2020-12-03 22:33:46 +0530

    Chat App

--- chat_section.php
+++ chat_section.php
@@ -9,6 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
-<button id="down_button" style="margin:auto;border-radius:50%;position:fixed;bottom:40%;right:3.6%;"  type="submit" class="btn btn-outline-light"><i class="fas fa-arrow-down down_arrow"></i></button>
-<div  id="chat_content" style="height:70vh;overflow-y:auto;border-radius:10px;width:70vw;border:2px solid #fff;margin:1.3rem auto;position:relative;">
+<button id="down_button" style="margin:auto;border-radius:50%;position:fixed;bottom:40%;right:3.6%;"  type="submit" class="btn btn-outline-light shadow"><i class="fas fa-arrow-down down_arrow"></i></button>
+<div class="shadow"  id="chat_content" style="height:70vh;overflow-y:auto;border-radius:10px;width:70vw;border:2px solid #fff;margin:1.3rem auto;position:relative;">
 </div>
